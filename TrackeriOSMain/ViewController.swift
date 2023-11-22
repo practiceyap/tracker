edcae5289583ef144baef0727ada6b84bfd9c57e
practiceyap <sashastronk4@gmail.com>
2023-11-23 00:10:02 +0600

    Initial Commit

---
+++ TrackeriOSMain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackeriOSMain
+//
+//  Created by Muller Alexander on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
